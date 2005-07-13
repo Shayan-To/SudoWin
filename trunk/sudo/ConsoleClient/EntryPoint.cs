3281ee71276222d9bd49615f47bebcce87314242
akutz <akutz@7db70799-3c16-0410-93a7-84a21c41fa1b>
2005-07-13 01:32:01 +0000

    changed the directory Client to ConsoleClient

---
+++ trunk/sudo/ConsoleClient/EntryPoint.cs
@@ -0,0 +1,83 @@
+/*
+Copyright (c) 2005, Schley Andrew Kutz <sakutz@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+    * Neither the name of Lost Creations nor the names of its contributors may
+    be used to endorse or promote products derived from this software without
+    specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
+
+using Sudo.Shared;
+
+using System.Reflection;
+
+using System.Runtime.Remoting;
+
+namespace Sudo.ConsoleClient
+{
+	class EntryPoint
+	{
+		static void Main( string[] args )
+		{
+			// get path to the actual exe
+			Uri uri = new Uri(
+				Assembly.GetExecutingAssembly().GetName().CodeBase );
+
+			// configure remoting channels and objects
+			RemotingConfiguration.Configure( uri.LocalPath + ".config" );
+
+			// get the server object that is used to elevate
+			// privleges and act as a backend store for
+			// caching credentials
+
+			// get an array of the registered well known client urls
+			WellKnownClientTypeEntry[] wkctes =
+				RemotingConfiguration.GetRegisteredWellKnownClientTypes();
+
+			// loop through the list of well known clients until
+			// the SudoServer object is found
+			ISudoServer iss = null;
+			for ( int x = 0; x < wkctes.Length && iss == null; ++x )
+			{
+				iss = Activator.GetObject( typeof( ISudoServer ),
+					wkctes[ x ].ObjectUrl ) as ISudoServer;
+			}
+
+			// set the command name to sudo
+			string command_name =
+				args.Length < 1 ?
+				string.Empty : args[ 0 ];
+
+			// set the command arguments to sudo
+			string command_args =
+				args.Length < 2 ?
+				string.Empty :
+				string.Join( " ", args, 1, args.Length - 1 );
+
+			// invoke sudo!
+			Sudo.Shared.Client.Sudo(
+				iss, command_name, command_args );
+		}
+	}
+}
