704f3e9efdd4a507e5f6fd04c7493468e4c9f5f2
akutz <akutz@7db70799-3c16-0410-93a7-84a21c41fa1b>
2006-06-24 05:06:42 +0000

    - index.html - fixed spelling mistake - changed 'lease' to 'least' in Least User Access Mode
    - Controller.Designer.cs - set the class level field controller to null so that VS does not complain about an unassigned field

--- trunk/sudowin/WindowsService/Controller.Designer.cs
+++ trunk/sudowin/WindowsService/Controller.Designer.cs
@@ -36,7 +36,7 @@ namespace Sudowin.WindowsService
 		/// <summary> 
 		///		Required designer variable.
 		/// </summary>
-		private System.ComponentModel.IContainer components ;
+		private System.ComponentModel.IContainer components = null;
 
 		/// <summary>
 		///		Clean up any resources being used.
