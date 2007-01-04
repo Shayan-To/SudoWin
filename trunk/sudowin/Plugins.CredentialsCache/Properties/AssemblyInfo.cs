7427e14c90f853fe7f48c3e6dbf469a3f5d46d58
akutz <akutz@7db70799-3c16-0410-93a7-84a21c41fa1b>
2007-01-04 03:56:26 +0000

    - changed the copyright notice to include 2007

--- trunk/sudowin/Plugins.CredentialsCache/Properties/AssemblyInfo.cs
+++ trunk/sudowin/Plugins.CredentialsCache/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2005, 2006, Schley Andrew Kutz <akutz@lostcreations.com>
+Copyright (c) 2005, 2006, 2007, Schley Andrew Kutz <akutz@lostcreations.com>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
