2c14489924c4f6f1984fd6fdf64462b057a104f3
kiliman <kiliman@7db70799-3c16-0410-93a7-84a21c41fa1b>
2008-01-14 21:22:51 +0000

    - update version number to 0.5.

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -45,8 +45,8 @@ using System.Runtime.CompilerServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
 
 
 #if DEBUG
